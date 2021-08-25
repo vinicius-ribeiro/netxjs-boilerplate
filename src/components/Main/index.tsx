1d7485f456bd6948fac54fc3f76aa83265ed64b6
Vinicius Ribeiro <vy_ribeiro@yahoo.com.br>
2021-08-25 19:52:44 -0300

    Correcoes aula 23

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imatem de um átomo e React Avançado escrito ao lado"
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      Typescript, ReactJS, NextJs e Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Desenvolvedor de frente para a tela com código"
